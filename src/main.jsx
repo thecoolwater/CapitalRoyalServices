ebef41b26a11e27ca34321830ab885edd629027f
thecoolwater <fdemo35@gmail.com>
2026-09-08 16:56:39 +0530

    Configure custom domain for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/CapitalRoyalServices">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
